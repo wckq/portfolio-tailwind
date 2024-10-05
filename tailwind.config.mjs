bdd652629aac8d39f90bb63de2731b1cf471e007
wckq <wickedyour442@gmail.com>
2024-10-05 14:30:45 -0600

    feat: initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"white": "#F6F6F6",
+			"purple": {
+				400: "#968DFF",
+				700: "#8277FF"
+			},
+			"dark-gray": {
+				400: "262626",
+				700: "#1A1A1A"
+			}
+		},
+		fontFamily: {
+			sans: ['Onest', "sans-serif"]
+		},
+		extend: {},
+	},
+	plugins: [],
+}
