9113462dc9d56fa9cd56c4b450010c7f88007968
wckq <wickedyour442@gmail.com>
2024-10-06 13:37:08 -0600

    feat: added skills section

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,34 +1,88 @@
-// Importa el icono de Java
+import AmazonAWS from "@/components/icons/AmazonAWS.astro";
+import AstroLogo from "@/components/icons/AstroLogo.astro";
+import CSS from "@/components/icons/CSS.astro";
+import Express from "@/components/icons/Express.astro";
+import HTML from "@/components/icons/HTML.astro";
 import Java from "@/components/icons/Java.astro";
+import JavaScript from "@/components/icons/JavaScript.astro";
+import MongoDB from "@/components/icons/MongoDB.astro";
+import NodeJS from "@/components/icons/NodeJS.astro";
+import PostgreSQL from "@/components/icons/PostgreSQL.astro";
+import React from "@/components/icons/React.astro";
+import Redis from "@/components/icons/Redis.astro";
+import SpringBoot from "@/components/icons/SpringBoot.astro";
+import Typescript from "@/components/icons/Typescript.astro";
+
 import type { AstroComponentFactory } from "astro/runtime/server/index.js";
 
-// Define un tipo para los lenguajes
-type Language = "JAVA";
+type Language = "HTML" | "CSS" | "JAVASCRIPT" | "JAVA" | "TYPESCRIPT"
+type Technologies = "REDIS" | "ASTRO" | "REACT" | "EXPRESS" | "SPRING_BOOT" | "NODE_JS" | "POSTGRESQL" | "MONGO_DB" | "AMAZON_AWS"
 
-// Define la interfaz Raw para los lenguajes y tecnologías
-interface Raw {
+export interface Raw {
     name: string;
-    description: string;
     logo: AstroComponentFactory;
 }
 
-export const LANGUAGES: Record<Language, Raw> = {
+export const TECHNOLOGIES: Record<Language | Technologies, Raw> = {
     JAVA: {
         name: "Java",
-        description: "A versatile programming language.",
         logo: Java,
     },
-};
-
-export const TECHNOLOGIES: Record<string, Raw> = {
-    java: {
-        name: "Java",
-        description: "A versatile programming language.",
-        logo: Java,
+    REDIS: {
+        name: "Redis",
+        logo: Redis
     },
+    TYPESCRIPT: {
+        name: "Typescript",
+        logo: Typescript
+    },
+    MONGO_DB: {
+        name: "MongoDB",
+        logo: MongoDB
+    },
+    HTML: {
+        name: "HTML",
+        logo: HTML
+    },
+    CSS: {
+        name: "CSS",
+        logo: CSS
+    },
+    AMAZON_AWS: {
+        name: "Amazon AWS",
+        logo: AmazonAWS
+    },
+    ASTRO: {
+        name: "Astro",
+        logo: AstroLogo
+    },
+    EXPRESS: {
+        name: "Express",
+        logo: Express
+    },
+    JAVASCRIPT: {
+        name: "JavaScript",
+        logo: JavaScript
+    },
+    NODE_JS: {
+        name: "NodeJS",
+        logo: NodeJS
+    },
+    POSTGRESQL: {
+        name: "PostgreSQL",
+        logo: PostgreSQL
+    },
+    REACT: {
+        name: "React",
+        logo: React
+    },
+    SPRING_BOOT: {
+        name: "Spring Boot",
+        logo: SpringBoot
+    }
 };
 
-// Define la interfaz Projects
+
 export interface Projects {
     name: string;
     description: string;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,14 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
-			"white": "#F6F6F6",
+			"white": {
+				DEFAULT: "#F6F6F6",
+				700: "#e1e1e1"
+			},
 			"purple": {
 				400: "#968DFF",
 				700: "#8277FF"
 			},
 			"dark-gray": {
 				400: "#262626",
-				700: "#1A1A1A"
+				700: "#1A1A1A",
+				800: "#0f0f0f"
 			}
 		},
 		fontFamily: {
